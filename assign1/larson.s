3a31811353db24a25d75f4cdcc9e4129213c41c6
Jared Weissberg <jared1@stanford.edu>
2024-10-03 19:51:08 -0700

    got four LEDs to blink together

--- assign1/larson.s
+++ assign1/larson.s
@@ -9,17 +9,21 @@
  * Be sure to use GPIO pins PB0-PB3 (or PB0-PB7) for your LEDs.
  */
 
-    lui     a0,0x2000       # a0 holds base addr PB group = 0x2000000
-    addi    a1,zero,1       # a1 holds constant 1
-    sw      a1,0x30(a0)     # config PB0 as output
+    lui     a0,0x2000        # a0 holds base addr PB group = 0x2000000
+    li      a1,0x1111        # we create the binary 0001 x 4 = 0x1111
+    sw      a1,0x30(a0)      # config PB0/1/2/3 as outputs instead of inputs by storing 0x1111
 
- loop:
-    xori    a1,a1,1         # xor ^ 1 invert a1
-    sw      a1,0x40(a0)     # set data value of PB0 to a1
+    addi    a2,a2,15         # create a value a2 that will turn on the pins via binary 1111
 
-    lui     a2,0x4500       # a2 = init countdown value
- delay:
-    addi    a2,a2,-1        # decrement a2
-    bne     a2,zero,delay   # keep counting down until a2 is zero
 
-    j       loop            # back to top of outer loop
+    loop:
+      xori    a2,a2,15       # invert a1 for all binary 1
+      sw      a2,0x40(a0)    # set data value of PB0/1/2/3 to a2; this tells the pins to emit 3.3V when 1 or 0V when 0
+      lui     a3,0x4500      # a3 = init countdown value
+   
+    delay:
+      addi    a3,a3,-1       # decrement a3
+      bne     a3,zero,delay  # keep counting down until a3 is zero
+
+      j       loop           # back to top of outer loop
+
